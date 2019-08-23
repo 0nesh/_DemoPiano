66a3100666215e3fde846bea4db3f4b5cc06448c
0nesh <54450602+0nesh@users.noreply.github.com>
2019-08-24 00:50:50 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace _DemoPiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pBG = new System.Windows.Forms.PictureBox();
+            this.pBF = new System.Windows.Forms.PictureBox();
+            this.pBE = new System.Windows.Forms.PictureBox();
+            this.pBD = new System.Windows.Forms.PictureBox();
+            this.pBC = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnF = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.pBA = new System.Windows.Forms.PictureBox();
+            this.pBB = new System.Windows.Forms.PictureBox();
+            this.paneRecording = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(671, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(704, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "D";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(736, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "E";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(671, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "F";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(704, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(28, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "G";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoEllipsis = true;
+            this.button6.Location = new System.Drawing.Point(736, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(28, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "A";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(671, 9);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(28, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "B";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pBB);
+            this.panel1.Controls.Add(this.pBA);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pBG);
+            this.panel1.Controls.Add(this.pBF);
+            this.panel1.Controls.Add(this.pBE);
+            this.panel1.Controls.Add(this.pBD);
+            this.panel1.Controls.Add(this.pBC);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 207);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 143);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_DemoPiano.Properties.Resources.not_nhac;
+            this.pictureBox2.Location = new System.Drawing.Point(18, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(87, 86);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pBG
+            // 
+            this.pBG.Image = global::_DemoPiano.Properties.Resources.note;
+            this.pBG.Location = new System.Drawing.Point(358, 50);
+            this.pBG.Name = "pBG";
+            this.pBG.Size = new System.Drawing.Size(13, 14);
+            this.pBG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBG.TabIndex = 5;
+            this.pBG.TabStop = false;
+            // 
+            // pBF
+            // 
+            this.pBF.Image = global::_DemoPiano.Properties.Resources.note;
+            this.pBF.Location = new System.Drawing.Point(312, 58);
+            this.pBF.Name = "pBF";
+            this.pBF.Size = new System.Drawing.Size(13, 14);
+            this.pBF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBF.TabIndex = 4;
+            this.pBF.TabStop = false;
+            // 
+            // pBE
+            // 
+            this.pBE.Image = global::_DemoPiano.Properties.Resources.note;
+            this.pBE.Location = new System.Drawing.Point(268, 63);
+            this.pBE.Name = "pBE";
+            this.pBE.Size = new System.Drawing.Size(13, 14);
+            this.pBE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBE.TabIndex = 3;
+            this.pBE.TabStop = false;
+            // 
+            // pBD
+            // 
+            this.pBD.Image = global::_DemoPiano.Properties.Resources.note;
+            this.pBD.Location = new System.Drawing.Point(223, 70);
+            this.pBD.Name = "pBD";
+            this.pBD.Size = new System.Drawing.Size(13, 14);
+            this.pBD.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBD.TabIndex = 2;
+            this.pBD.TabStop = false;
+            // 
+            // pBC
+            // 
+            this.pBC.Image = global::_DemoPiano.Properties.Resources.mediumcfffbafe_1446182847n4kg81;
+            this.pBC.Location = new System.Drawing.Point(177, 74);
+            this.pBC.Name = "pBC";
+            this.pBC.Size = new System.Drawing.Size(13, 10);
+            this.pBC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBC.TabIndex = 1;
+            this.pBC.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_DemoPiano.Properties.Resources.line;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(745, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.SystemColors.Control;
+            this.btnC.Location = new System.Drawing.Point(179, 109);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(44, 95);
+            this.btnC.TabIndex = 8;
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(222, 109);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(44, 95);
+            this.btnD.TabIndex = 9;
+            this.btnD.UseVisualStyleBackColor = true;
+            // 
+            // btnE
+            // 
+            this.btnE.Location = new System.Drawing.Point(265, 109);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(44, 95);
+            this.btnE.TabIndex = 10;
+            this.btnE.UseVisualStyleBackColor = true;
+            // 
+            // btnF
+            // 
+            this.btnF.Location = new System.Drawing.Point(308, 109);
+            this.btnF.Name = "btnF";
+            this.btnF.Size = new System.Drawing.Size(44, 95);
+            this.btnF.TabIndex = 11;
+            this.btnF.UseVisualStyleBackColor = true;
+            // 
+            // btnG
+            // 
+            this.btnG.Location = new System.Drawing.Point(351, 109);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(44, 95);
+            this.btnG.TabIndex = 12;
+            this.btnG.UseVisualStyleBackColor = true;
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(437, 109);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(44, 95);
+            this.btnB.TabIndex = 14;
+            this.btnB.UseVisualStyleBackColor = true;
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(394, 109);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(44, 95);
+            this.btnA.TabIndex = 13;
+            this.btnA.UseVisualStyleBackColor = true;
+            // 
+            // pBA
+            // 
+            this.pBA.Image = global::_DemoPiano.Properties.Resources.mediumcfffbafe_1446182847n4kg81;
+            this.pBA.Location = new System.Drawing.Point(399, 46);
+            this.pBA.Name = "pBA";
+            this.pBA.Size = new System.Drawing.Size(13, 10);
+            this.pBA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBA.TabIndex = 7;
+            this.pBA.TabStop = false;
+            // 
+            // pBB
+            // 
+            this.pBB.Image = global::_DemoPiano.Properties.Resources.mediumcfffbafe_1446182847n4kg81;
+            this.pBB.Location = new System.Drawing.Point(441, 39);
+            this.pBB.Name = "pBB";
+            this.pBB.Size = new System.Drawing.Size(13, 10);
+            this.pBB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBB.TabIndex = 8;
+            this.pBB.TabStop = false;
+            // 
+            // paneRecording
+            // 
+            this.paneRecording.Location = new System.Drawing.Point(640, 109);
+            this.paneRecording.Name = "paneRecording";
+            this.paneRecording.Size = new System.Drawing.Size(148, 100);
+            this.paneRecording.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(800, 350);
+            this.Controls.Add(this.paneRecording);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pBC;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pBG;
+        private System.Windows.Forms.PictureBox pBF;
+        private System.Windows.Forms.PictureBox pBE;
+        private System.Windows.Forms.PictureBox pBD;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnF;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.PictureBox pBB;
+        private System.Windows.Forms.PictureBox pBA;
+        private System.Windows.Forms.Panel paneRecording;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _DemoPiano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ShowNote();
+            ShowBColor();
+        }
+        //Show Hide Note
+        private void ShowNote()
+        {
+            pBC.Hide();
+            pBD.Hide();
+            pBE.Hide();
+            pBF.Hide();
+            pBG.Hide();
+            pBA.Hide();
+            pBB.Hide();
+            
+        }
+        //đổi màu note
+        private void ShowBColor()
+        {
+            this.btnC.BackColor = Color.White;
+            this.btnD.BackColor = Color.White;
+            this.btnE.BackColor = Color.White;
+            this.btnF.BackColor = Color.White;
+            this.btnG.BackColor = Color.White;
+            this.btnA.BackColor = Color.White;
+            this.btnB.BackColor = Color.White;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad1:
+                    button1.PerformClick();
+                    break;
+                case Keys.NumPad2:
+                    button2.PerformClick();
+                    break;
+                case Keys.NumPad3:
+                    button3.PerformClick();
+                    break;
+                case Keys.NumPad4:
+                    button4.PerformClick();
+                    break;
+                case Keys.NumPad5:
+                    button5.PerformClick();
+                    break;
+                case Keys.NumPad6:
+                    button6.PerformClick();
+                    break;
+                case Keys.NumPad7:
+                    button7.PerformClick();
+                    break;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        
+        //Random random = new Random();
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            ShowNote();
+            ShowBColor();
+            this.btnD.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "D.WAV";
+            player.controls.play();
+            pBD.Show();
+            this.btnC.BackColor = Color.White;
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            pBE.Show();
+            ShowBColor();
+            this.btnE.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "E.WAV";
+            player.controls.play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            pBF.Show();
+            ShowBColor();
+            this.btnF.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "F.WAV";
+            player.controls.play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            pBG.Show();
+            ShowBColor();
+            this.btnG.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "G.WAV";
+            player.controls.play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            pBA.Show();
+            ShowBColor();
+            this.btnA.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "A.WAV";
+            player.controls.play();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            pBB.Show();
+            ShowBColor();
+            this.btnB.BackColor = Color.FromArgb(78, 184, 206);    
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "B.WAV";
+            player.controls.play();
+        }
+
+        int x = 710, y = 76, a = 5;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int width = this.Width;
+            if (pBC.Location.X > width - pBC.Width)
+            {
+                pBC.Location = new Point(1, pBC.Location.Y);
+            }
+            else
+            {
+                pBC.Location = new Point(pBC.Location.X + 100, pBC.Location.Y);
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            ShowNote();
+            ShowBColor();
+            this.btnC.BackColor = Color.FromArgb(78, 184, 206);
+            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+            player.URL = "C.WAV";
+            player.controls.play();
+            pBC.Show();
+        }
+    }
+}
